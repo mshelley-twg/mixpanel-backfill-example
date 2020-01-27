f4dacf43d2bb87c077b51070ed2e4b33cd3c398b
Matt Shelley <mshelley@theworkinggroup.ca>
2020-01-27 17:07:14 -0500

    Set an environment variable, and rename app.js to server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(`Hello World! ${process.env.TOKEN}`))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
