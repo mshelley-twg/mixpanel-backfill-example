70e3a736563873409a31ec0bd78d9e3e83aa5d65
Matt Shelley <mshelley@theworkinggroup.ca>
2020-01-28 14:37:11 -0500

    Set mock data for one user

--- routes/backfill.js
+++ routes/backfill.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const base64 = require('base-64')
 
 const router = express.Router()
@@ -9,13 +10,13 @@ router.post('/', (req, res) => {
 })
 
 // Backfill a single user
-router.post('/:userId', (req, res) => {
-  console.log(`Backfill user with id: ${req.params.userId}`)
+router.post('/:userId', async (req, res, next) => {
+  const { userId } = req.params
 
   // Create the change request
   const changeRequest = {
-    $token: process.env.TOKEN,
-    $distinct_id: '13793',
+    $token: process.env.MIXPANEL_TOKEN,
+    $distinct_id: userId,
     // Do not update "Last Seen" time for user
     $ignore_time: true,
     $set: {
@@ -23,10 +24,24 @@ router.post('/:userId', (req, res) => {
     }
   }
 
-  // Base64-encode the JSON string
+  // Encode the string to pass as a query to Mixpanel
   const encodedChangeRequest = base64.encode(JSON.stringify(changeRequest))
 
-  res.send(`https://api.mixpanel.com/engage/?data=${encodedChangeRequest}`)
+  // Create the Mixpanel request URL
+  const mixpanelRequestUrl = `https://api.mixpanel.com/engage/?data=${encodedChangeRequest}`
+
+  try {
+    const { data } = await axios.post(mixpanelRequestUrl)
+
+    // Mixpanel returns 1 for success, and 0 for failure
+    if (data === 1) {
+      res.status(201).send('Successfully updated a Mixpanel user profile')
+    } else {
+      throw new Error('Mixpanel returned failure code')
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
